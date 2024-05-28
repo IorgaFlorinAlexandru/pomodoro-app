98deba65753e3bd38e94dca294620762a0ede053
IorgaFlorinAlexandru <iorgaflorinalexandru@gmail.com>
2024-05-28 21:08:51 +0300

    Added a mock up select for the settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -5,6 +5,7 @@ function Settings() {
 
     const [sessionAlarm, setSessionAlarm] = useState(false);
     const [breakAlarm, setBreakAlarm] = useState(false);
+    const [sendNotifications, setSendNotifications] = useState(false);
 
     function toggleSessionAlarm(value: boolean): void {
         setSessionAlarm(value);
@@ -14,27 +15,47 @@ function Settings() {
         setBreakAlarm(value);
     }
 
+    function toggleNotifications(value: boolean): void {
+        setSendNotifications(value);
+    }
+
     return (
      <>
       <div className="flex flex-col p-8">
        <h2 className="text-4xl">Settings</h2>
        <div className="mt-8">
         <h2 className="text-xl mb-2">Focus Sessions</h2>
-        <div className="w-full my-1 p-2 px-4 rounded bg-gruvbox-gray-600">
+        <div className="flex justify-between items-center w-full my-1 p-2 px-4 
+            rounded bg-gruvbox-gray-600">
          <div>
             <h4>Focus Time</h4>
             <p className="text-[0.8rem] text-gruvbox-wheat-500">
                 Adjust the lengths of your focus time
             </p>
          </div>
+         <select className="w-32 h-8 pl-2 rounded text-gruvbox-wheat-200
+            bg-gruvbox-gray-500 hover:bg-gruvbox-gray-400" defaultValue="1500" >
+            <option value="1500">25 minutes</option>
+            <option value="2000">35 minutes</option>
+            <option value="3000">1 hour</option>
+            <option value="6000">1h 30min</option>
+         </select>
         </div>
-        <div className="w-full my-1 p-2 px-4 rounded bg-gruvbox-gray-600">
+        <div className="flex justify-between items-center 
+            w-full my-1 p-2 px-4 rounded bg-gruvbox-gray-600">
          <div>
             <h4>Break Time</h4>
             <p className="text-[0.8rem] text-gruvbox-wheat-500">
                 Adjust the lengths of your break time
             </p>
          </div>
+         <select className="w-32 h-8 pl-2 rounded text-gruvbox-wheat-200
+            bg-gruvbox-gray-500 hover:bg-gruvbox-gray-400" defaultValue="1500" >
+            <option value="1500">5 minutes</option>
+            <option value="2000">10 minutes</option>
+            <option value="3000">30 minutes</option>
+            <option value="6000">1 hour</option>
+         </select>
         </div>
         <div className="flex justify-between items-center w-full my-1 p-2 px-4 
             rounded bg-gruvbox-gray-600">
@@ -65,20 +86,32 @@ function Settings() {
        </div>
        <div className="mt-8">
         <h2 className="text-xl mb-2">General</h2>
-        <div className="w-full my-1 p-2 px-4 rounded bg-gruvbox-gray-600">
+        <div className="flex justify-between items-center
+            w-full my-1 p-2 px-4 rounded bg-gruvbox-gray-600">
          <div>
             <h4>App Theme</h4>
             <p className="text-[0.8rem] text-gruvbox-wheat-500">
                Change the app theme
             </p>
          </div>
+         <select className="w-44 h-8 pl-2 rounded text-gruvbox-wheat-200
+            bg-gruvbox-gray-500 hover:bg-gruvbox-gray-400" defaultValue="0" >
+            <option value="0">Gruvbox</option>
+            <option value="1">Solarized White</option>
+            <option value="2">Solarized Dark</option>
+         </select>
         </div>
-        <div className="w-full my-1 p-2 px-4 rounded bg-gruvbox-gray-600">
+        <div className="flex justify-between items-center
+            w-full my-1 p-2 px-4 rounded bg-gruvbox-gray-600">
          <div>
             <h4>Notifications</h4>
             <p className="text-[0.8rem] text-gruvbox-wheat-500">
                Send a notification when the focus or break period ends 
             </p>
+         </div>
+         <div className="flex items-center">
+          <span className="mr-4">{sendNotifications ? 'On' : 'Off'}</span>
+          <ToggleSwitch value={sendNotifications} onChange={toggleNotifications}/>
          </div>
         </div>
        </div>
